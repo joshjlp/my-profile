994f51c6f54504e763e8bc08113573a0b8b20e61
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2023-11-23 19:23:17 +0800

    Code changes.

--- src/Client/Features/ChatGpt/Constants/ChatGptConstants.cs
+++ src/Client/Features/ChatGpt/Constants/ChatGptConstants.cs
@@ -5,5 +5,5 @@ public static class ChatGptConstants
     public const string BaseAddress = "https://obaki-webapi.onrender.com";
     public const string CacheDataKey = "chat-gpt-response-cachedata2";
     public const string Endpoint = "/ask-chatgpt";
-    public const string Question = " create a lively introduction with this information `Josh  .NET Developer Hey there, I'm a developer who has a passion for acquiring fresh knowledge and crafting innovative tools and applications. Having worked for 11 years, I possess expertise in various technologies, but I'm constantly seeking challenging opportunities that enable me to step beyond my comfort zone. ` limit it to 3 paragraphs.  Be funny and make some jokes.";
+    public const string Question = " create a lively introduction with this information `Josh  .NET Developer Hey there, I'm a developer who has a passion for acquiring fresh knowledge and crafting innovative tools and applications. Having worked for 11 years, I possess expertise in various technologies, but I'm constantly seeking challenging opportunities that enable me to step beyond my comfort zone. ` limit it to 3 paragraphs.  Be funny and make some jokes then make sure to respond politely .";
 }
